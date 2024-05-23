55d7454be888b2fadf78647fb9f5aa8b3a718198
Ehud Ettun <ehudettun@gmail.com>
2024-05-23 10:31:04 -0400

    basic chart functionality and normalized graph

--- backend/market-ticker/src/main/java/com/example/marketticker/MarketTickerApplication.java
+++ backend/market-ticker/src/main/java/com/example/marketticker/MarketTickerApplication.java
@@ -2,6 +2,9 @@ package com.example.marketticker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MarketTickerApplication {
@@ -9,5 +12,18 @@ public class MarketTickerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MarketTickerApplication.class, args);
 	}
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+				.allowedOrigins("*")
+				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow specified HTTP methods
+                .allowedHeaders("*"); // Allow all headers
+			}
+		};
+	}
+
 
 }
